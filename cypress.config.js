2ad7b34c9357cec846e4891a8ec1d8ee401220ad
Denys Naumenko <d.naumenko1997@gmail.com>
2023-12-13 22:42:26 +0200

    fix 2

--- cypress.config.js
+++ cypress.config.js
@@ -30,7 +30,7 @@ module.exports = defineConfig({
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/results',
-      overwrite: false,
+      overwrite: true,
       html: false,
       json: true,
     }
